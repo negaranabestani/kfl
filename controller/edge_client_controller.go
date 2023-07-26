e9da88b75e4f44dac4b063ef17d84b604ce80a77
negar anabestani <negar.anabestani@gmail.com>
2023-07-26 14:09:06 +0430

    add required controller files

---
+++ controller/edge_client_controller.go
@@ -0,0 +1,16 @@
+package controller
+
+import (
+	"context"
+	"github.com/negaranabestani/kfl/api/v1alpha1"
+)
+
+func (r *FLClusterReconciler) createOrUpdateEdgeClient(ctx context.Context, cluster v1alpha1.FLCluster) error {
+	//TODO implement and add required input params
+	return nil
+}
+
+func (r *FLClusterReconciler) deleteEdgeClient(ctx context.Context, cluster v1alpha1.FLCluster) error {
+	//TODO implement and add required input params
+	return nil
+}
