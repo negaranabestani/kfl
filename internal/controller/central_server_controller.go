31a6da0e79bfe4e8b39ae01deabe576ef08e1899
negar anabestani <negar.anabestani@gmail.com>
2023-07-31 17:37:27 +0430

    set controller reference to central server deployment

--- internal/controller/central_server_controller.go
+++ internal/controller/central_server_controller.go
@@ -7,6 +7,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 const (
@@ -30,7 +31,11 @@ func (r *FLClusterReconciler) centralServerDesiredDeployment(cluster *v1alpha1.F
 	resources, _ := utils.ResourceRequirements(cluster.Spec.CentralServer.Resources)
 	dep := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: cluster.Name + "-central-server-deployment",
+			Name:      cluster.Name + "-central-server-deployment",
+			Namespace: cluster.Namespace,
+			Labels: map[string]string{
+				"app": CentralServerSelectorApp,
+			},
 		},
 		Spec: appsv1.DeploymentSpec{
 			Replicas: utils.Int32ptr(cluster.Spec.CentralServer.Replica),
@@ -62,6 +67,9 @@ func (r *FLClusterReconciler) centralServerDesiredDeployment(cluster *v1alpha1.F
 				},
 			},
 		},
+	}
+	if err := ctrl.SetControllerReference(cluster, dep, r.Scheme); err != nil {
+		return dep, err
 	}
 	return dep, nil
 }
