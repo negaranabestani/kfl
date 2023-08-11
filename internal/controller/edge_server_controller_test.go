f5d83807a3b8b052161f56b35ebbe7103f88364e
negar anabestani <negar.anabestani@gmail.com>
2023-08-11 16:49:54 +0430

    add edge server create or update test

--- internal/controller/edge_server_controller_test.go
+++ internal/controller/edge_server_controller_test.go
@@ -1,15 +1,64 @@
 package controller
 
 import (
+	"context"
 	"github.com/negaranabestani/kfl/api/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"testing"
 )
+
+func edgeServerCreateOrUpdateTest(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	_ = appsv1.AddToScheme(scheme)
+	_ = v1alpha1.AddToScheme(scheme)
+
+	flCluster := &v1alpha1.FLCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-fl",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.FLClusterSpec{
+			EdgeServer: v1alpha1.Device{
+				Replica: 1,
+				Resources: v1alpha1.Resources{
+					Cpu:    "1000m",
+					Memory: "128Mi",
+				},
+			},
+		},
+	}
+	r := &FLClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(scheme).WithObjects(flCluster).Build(),
+		Scheme: scheme,
+	}
+	err := r.createOrUpdateEdgeServer(context.Background(), flCluster)
+	if err != nil {
+		t.Fatalf("failed to create or update edge server")
+	}
+	deployment := &appsv1.Deployment{}
+	err1 := r.Get(context.Background(), client.ObjectKey{
+		Name:      flCluster.Name + "-edge-server",
+		Namespace: flCluster.Namespace,
+	}, deployment)
+	if err1 != nil {
+		t.Errorf("failed to get deployment")
+	}
+	service := &corev1.Service{}
+	err2 := r.Get(context.Background(), client.ObjectKey{
+		Name:      flCluster.Name + "-edge-server",
+		Namespace: flCluster.Namespace,
+	}, service)
+	if err2 != nil {
+		t.Errorf("failed to get service")
+	}
+}
 
 func EdgeServerDesiredDeploymentTest(t *testing.T) {
 	scheme := runtime.NewScheme()
