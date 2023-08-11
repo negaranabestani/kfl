c99fb1bc9727fbf58f88dbe438b4f24b519c416c
negar anabestani <negar.anabestani@gmail.com>
2023-08-11 16:33:12 +0430

    add edge server desired deployment and service

--- internal/controller/edge_server_controller.go
+++ internal/controller/edge_server_controller.go
@@ -3,6 +3,21 @@ package controller
 import (
 	"context"
 	"github.com/negaranabestani/kfl/api/v1alpha1"
+	utils "github.com/negaranabestani/kfl/internal/util"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+const (
+	edgeServer                  = "edge-server"
+	edgeServerSelectorApp       = "edge-server"
+	edgeServerImage             = ""
+	edgeServerContainerPort     = 8080
+	edgeServerContainerPortName = "httpbin"
+	edgeServerServicePort       = 8080
 )
 
 func (r *FLClusterReconciler) createOrUpdateEdgeServer(ctx context.Context, cluster v1alpha1.FLCluster) error {
@@ -13,4 +28,86 @@ func (r *FLClusterReconciler) createOrUpdateEdgeServer(ctx context.Context, clus
 func (r *FLClusterReconciler) deleteEdgeServer(ctx context.Context, cluster v1alpha1.FLCluster) error {
 	//TODO implement and add required input params
 	return nil
+}
+
+func (r *FLClusterReconciler) edgeServerDesiredDeployment(cluster *v1alpha1.FLCluster) (*appsv1.Deployment, error) {
+	resources, _ := utils.ResourceRequirements(cluster.Spec.EdgeServer.Resources)
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cluster.Name + "-" + edgeServer,
+			Namespace: cluster.Namespace,
+			Labels: map[string]string{
+				"cluster": cluster.Name,
+				"app":     edgeServerSelectorApp,
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: utils.Int32ptr(cluster.Spec.EdgeServer.Replica),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"cluster": cluster.Name,
+					"app":     edgeServerSelectorApp,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"cluster": cluster.Name,
+						"app":     edgeServerSelectorApp,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  cluster.Name + "-" + edgeServer,
+							Image: edgeServerImage,
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          edgeServerContainerPortName,
+									ContainerPort: edgeServerContainerPort,
+								},
+							},
+							Resources: *resources,
+						},
+					},
+				},
+			},
+		},
+	}
+	if err := ctrl.SetControllerReference(cluster, dep, r.Scheme); err != nil {
+		return dep, err
+	}
+	return dep, nil
+}
+
+func (r *FLClusterReconciler) edgeServerDesiredService(cluster *v1alpha1.FLCluster) (*corev1.Service, error) {
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: cluster.Namespace,
+			Name:      cluster.Name + "-central-server",
+			Labels: map[string]string{
+				"cluster": cluster.Name,
+				"app":     edgeServerSelectorApp,
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "default",
+					Port:       int32(edgeServerServicePort),
+					TargetPort: intstr.FromString("default"),
+				},
+			},
+			Selector: map[string]string{
+				"cluster": cluster.Name,
+				"app":     edgeServerSelectorApp,
+			},
+		},
+	}
+
+	if err := ctrl.SetControllerReference(cluster, service, r.Scheme); err != nil {
+		return service, err
+	}
+
+	return service, nil
 }
