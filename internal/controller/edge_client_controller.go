4d73c7f760dbcfef2baff5407c65464e076fd41a
negar anabestani <negar.anabestani@gmail.com>
2023-08-14 14:19:22 +0430

    complete reconcile function

--- internal/controller/edge_client_controller.go
+++ internal/controller/edge_client_controller.go
@@ -5,7 +5,7 @@ import (
 	"github.com/negaranabestani/kfl/api/v1alpha1"
 )
 
-func (r *FLClusterReconciler) createOrUpdateEdgeClient(ctx context.Context, cluster v1alpha1.FLCluster) error {
+func (r *FLClusterReconciler) createOrUpdateEdgeClient(ctx context.Context, cluster *v1alpha1.FLCluster) error {
 	//TODO implement and add required input params
 	return nil
 }
