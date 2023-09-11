03452ec3e265f3ecd5198573eeb64040ed8ea1e8
negar anabestani <negar.anabestani@gmail.com>
2023-09-11 16:01:05 +0430

    add desired edge client service test

--- internal/controller/edge_client_controller_test.go
+++ internal/controller/edge_client_controller_test.go
@@ -23,7 +23,7 @@ func DesiredEdgeClientDeploymentTest(t *testing.T) {
 			Namespace: "sample",
 		},
 		Spec: v1alpha1.FLClusterSpec{
-			CentralServer: v1alpha1.Device{
+			EdgeClient: v1alpha1.Device{
 				Replica: 1,
 				Resources: v1alpha1.Resources{
 					Cpu:    "1",
@@ -60,4 +60,46 @@ func DesiredEdgeClientDeploymentTest(t *testing.T) {
 	if *deployment.Spec.Replicas != 1 {
 		t.Errorf("expected 1 central server deployment replica go %d", *deployment.Spec.Replicas)
 	}
+}
+
+func DesiredEdgeClientServiceTest(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	_ = appsv1.AddToScheme(scheme)
+	_ = v1alpha1.AddToScheme(scheme)
+
+	flCluster := &v1alpha1.FLCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-fl",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.FLClusterSpec{
+			EdgeClient: v1alpha1.Device{
+				Replica: 1,
+				Resources: v1alpha1.Resources{
+					Cpu:    "1",
+					Memory: "128Mi",
+				},
+			},
+		},
+	}
+	r := &FLClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(scheme).WithObjects(flCluster).Build(),
+		Scheme: scheme,
+	}
+
+	expectedName := flCluster.Name + "-" + EdgeClient
+	expectedNamespace := flCluster.Namespace
+	expectedLabels := map[string]string{
+		"cluster": flCluster.Name,
+		"app":     EdgeClientSelectorApp,
+	}
+
+	service, err := r.desiredEdgeClientService(flCluster)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedName, service.Name)
+	assert.Equal(t, expectedNamespace, service.Namespace)
+	assert.Equal(t, expectedLabels, service.Labels)
+	assert.Equal(t, expectedLabels, service.Spec.Selector)
 }
