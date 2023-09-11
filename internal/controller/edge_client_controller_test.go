e92b685f2244f64bbaa73fcd54f1eb694f7a6f3c
negar anabestani <negar.anabestani@gmail.com>
2023-09-11 16:14:34 +0430

    add create or update edge client test

--- internal/controller/edge_client_controller_test.go
+++ internal/controller/edge_client_controller_test.go
@@ -1,16 +1,64 @@
 package controller
 
 import (
+	"context"
 	"github.com/negaranabestani/kfl/api/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"testing"
 )
 
+func CreateOrUpdateEdgeClientTest(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	_ = appsv1.AddToScheme(scheme)
+	_ = v1alpha1.AddToScheme(scheme)
+
+	flCluster := &v1alpha1.FLCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-fl",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.FLClusterSpec{
+			EdgeClient: v1alpha1.Device{
+				Replica: 1,
+				Resources: v1alpha1.Resources{
+					Cpu:    "1",
+					Memory: "5Gi",
+				},
+			},
+		},
+	}
+	r := &FLClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(scheme).WithObjects(flCluster).Build(),
+		Scheme: scheme,
+	}
+	err := r.createOrUpdateEdgeClient(context.Background(), flCluster)
+	if err != nil {
+		t.Fatalf("failed to create or update edge client")
+	}
+	deployment := &appsv1.Deployment{}
+	err1 := r.Get(context.Background(), client.ObjectKey{
+		Name:      flCluster.Name + "-" + EdgeClient,
+		Namespace: flCluster.Namespace,
+	}, deployment)
+	if err1 != nil {
+		t.Errorf("failed to get deployment")
+	}
+	service := &corev1.Service{}
+	err2 := r.Get(context.Background(), client.ObjectKey{
+		Name:      flCluster.Name + "-" + EdgeClient,
+		Namespace: flCluster.Namespace,
+	}, service)
+	if err2 != nil {
+		t.Errorf("failed to get service")
+	}
+}
 func DesiredEdgeClientDeploymentTest(t *testing.T) {
 	scheme := runtime.NewScheme()
 	_ = corev1.AddToScheme(scheme)
@@ -78,7 +126,7 @@ func DesiredEdgeClientServiceTest(t *testing.T) {
 				Replica: 1,
 				Resources: v1alpha1.Resources{
 					Cpu:    "1",
-					Memory: "128Mi",
+					Memory: "5Gi",
 				},
 			},
 		},
